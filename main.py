b4f48209d886fafdbb2ea3b96b1a5e74a1434ec6
Robert Bucsis <rbucsis@gmail.com>
2026-05-24 10:46:20 -0600

    Building post print_jobs endpoint

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from fastapi import FastAPI, Request
+from typing import Annotated
+from fastapi import FastAPI, Request, Header, HTTPException
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import psycopg2 as pg
+from psycopg2.extras import RealDictCursor
+import os
+import auth
+
+load_dotenv()
 
 class Document(BaseModel):
     doc_xml: str
 
 app = FastAPI()
 
-def auth(api_key):
-    pass
+def connect():
+    return pg.connect(
+        database=os.getenv("PG_NAME"),
+        user=os.getenv("PG_USER"),
+        host=os.getenv("PG_HOST"),
+        password=os.getenv("PG_PASS")
+    )
 
 @app.post("/request_jobs")
 async def get_print_jobs(req: Request):
@@ -23,5 +36,26 @@ async def get_print_jobs(req: Request):
     return ""
 
 @app.post("/print_jobs")
-async def post_print_jobs():
-    pass
+async def post_print_jobs(Authorization: Annotated[str, Header()], req: Document):
+    print(Authorization)
+    api_key = Authorization.split(" ")[1] if "Bearer " in Authorization else None
+
+    if not api_key:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    
+    conn = connect()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute("SELECT * FROM esdp.users_v1")
+    users = cur.fetchall()
+    for user in users:
+        if (auth.checkData(api_key, user['api_key'])):
+            doc = req.doc_xml or None
+            cur.execute("INSERT INTO esdp.print_jobs (created_by, doc_xml) VALUES (%s, %s)", (user['id'], doc,))
+            conn.commit()
+            cur.close()
+            conn.close()
+            return "OK"
+    
+    cur.close()
+    conn.close()
+    raise HTTPException(status_code=401, detail="Unauthorized")

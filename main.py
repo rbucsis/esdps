0776d0076f6a26a2893856b957563ca0f227074e
Robert Bucsis <rbucsis@gmail.com>
2026-05-27 10:48:59 -0600

    Using pg.py and templating response

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
 from typing import Annotated
-from fastapi import FastAPI, Request, Header, HTTPException
+from fastapi import FastAPI, Request, Header, HTTPException, Response
 from pydantic import BaseModel
-from dotenv import load_dotenv
-import psycopg2 as pg
-from psycopg2.extras import RealDictCursor
-import os
+import pg
 import auth
-
-load_dotenv()
 
 class Document(BaseModel):
     doc_xml: str
 
 app = FastAPI()
-
-def connect():
-    return pg.connect(
-        database=os.getenv("PG_NAME"),
-        user=os.getenv("PG_USER"),
-        host=os.getenv("PG_HOST"),
-        password=os.getenv("PG_PASS")
-    )
 
 @app.post("/request_jobs")
 async def get_print_jobs(req: Request):
@@ -29,33 +16,70 @@ async def get_print_jobs(req: Request):
     data = dict(body)
     connection_type = data["ConnectionType"] if "ConnectionType" in data else None
     api_key = data['ID'] if "ID" in data else None
-    name = data["Name"] if "Name" in data else "Unknown"
+    name = data["Name"] if "Name" in data else None
+    if not api_key:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    
     if connection_type == "GetRequest":
-        print(client_ip, "-",connection_type)
-        return ""
+        user = auth.authorize(api_key)
+        if user:
+            response = ""
+            conn = pg.connect()
+            cur = conn.cursor()
+            cur.execute("INSERT INTO esdp.requests (device_ip, user_id) VALUES (%s, %s) RETURNING id, created_utc", (client_ip, user,))
+            job = cur.fetchone()
+            job_id = job[0]
+            timestamp = job[1]
+            print(timestamp.strftime("%Y-%m-%d %H:%M:%S"),"-",client_ip)
+            cur.execute("UPDATE esdp.pending_jobs_v1 SET record_status = 'Complete' WHERE created_utc <= %s AND created_by = %s RETURNING id, doc_xml", (timestamp,user,))
+            docs = cur.fetchall()
+            if docs:
+                response = '''<?xml version="1.0" encoding="utf-8"?>
+<PrintRequestInfo Version="2.00">'''
+                for doc in docs:
+                    id = doc[0]
+                    data = doc[1]
+                    tmp = f'''<ePOSPrint>
+<Parameter>
+<devid>local_printer</devid>
+<timeout>10000</timeout>
+<printjobid>{id[:23]}</printjobid>
+</Parameter>
+<PrintData>
+{data}
+</PrintData>
+</ePOSPrint>'''
+                    response = response + tmp
+                
+                response = response + "</PrintRequestInfo>"
+
+            conn.commit()
+            cur.close()
+            conn.close()
+            return Response(content=response, media_type="text/xml")
+        
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    elif connection_type == "SetResponse":
+        print(body)
+
     return ""
 
 @app.post("/print_jobs")
 async def post_print_jobs(Authorization: Annotated[str, Header()], req: Document):
-    print(Authorization)
     api_key = Authorization.split(" ")[1] if "Bearer " in Authorization else None
-
     if not api_key:
         raise HTTPException(status_code=401, detail="Unauthorized")
     
-    conn = connect()
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute("SELECT * FROM esdp.users_v1")
-    users = cur.fetchall()
-    for user in users:
-        if (auth.checkData(api_key, user['api_key'])):
-            doc = req.doc_xml or None
-            cur.execute("INSERT INTO esdp.print_jobs (created_by, doc_xml) VALUES (%s, %s)", (user['id'], doc,))
-            conn.commit()
-            cur.close()
-            conn.close()
-            return "OK"
+    user = auth.authorize(api_key)
+
+    if user:
+        doc = req.doc_xml
+        conn = pg.connect()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO esdp.print_jobs (created_by, doc_xml) VALUES (%s, %s)", (user, doc,))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return "OK"
     
-    cur.close()
-    conn.close()
     raise HTTPException(status_code=401, detail="Unauthorized")
